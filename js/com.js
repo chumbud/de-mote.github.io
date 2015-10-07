a5d567417fc49a4e4bf37e959f8a7210352a3339
PJ Rosa <demotednyk@gmail.com>
2015-10-06 22:09:51 -0400

    font changes, new full view, live tests

--- js/com.js
+++ js/com.js
@@ -1,24 +1,27 @@
 $(document).ready(function(){
-     $("#dropdown").click(function(){
-          $(".dropdown-menu").fadeToggle(200);
-     });
+	$("#dropdown").click(function(){
+		$(".dropdown-menu").fadeToggle(200);
+	});
 });
 $(window).scroll(function () {
-     var nav = $('nav'),
-         ul = $('nav ul a'),
-         logo = $('.logo'),
-         dropdown = $('.dropdown-menu'),
-         propic = $('.pic img'),
-         scroll = $(window).scrollTop();
+	var nav = $('nav'),
+		ul = $('nav ul a'),
+		logo = $('.logo'),
+		dropdown = $('.dropdown-menu'),
+		back = $('.back'),
+		propic = $('.pic img'),
+		scroll = $(window).scrollTop();
 
-     if (scroll >= 300)
-          nav.addClass('nav-resize'),
-               logo.addClass('resize'),
-               dropdown.addClass('dropdown-resize'),
-               ul.addClass('resize');
-     else
-          nav.removeClass('nav-resize'),
-               logo.removeClass('resize'),
-               dropdown.removeClass('dropdown-resize'),
-               ul.removeClass('resize');
+	if (scroll >= 300)
+		nav.addClass('nav-resize'),
+			logo.addClass('resize'),
+			dropdown.addClass('dropdown-resize'),
+			ul.addClass('resize'),
+			back.addClass('resize');
+	else
+		nav.removeClass('nav-resize'),
+			logo.removeClass('resize'),
+			back.removeClass('resize'),
+			dropdown.removeClass('dropdown-resize'),
+			ul.removeClass('resize');
 });
