bdf8fd78d16ef0c9430c6d49cf0f29af92a25bfa
PJ Rosa <demotednyk@gmail.com>
2016-03-22 20:58:09 -0400

    more testing with smoothstate

--- index/test/www/js/functions.js
+++ index/test/www/js/functions.js
@@ -5,7 +5,8 @@
 	var $body = $('html, body'),
 		content = $('#main').smoothState({
 			// Runs when a link has been activated
-
+			prefetch: true,
+			pageCacheSize: 4,
 			onStart: {
 				duration: 250, // Duration of our animation
 				render: function (url, $container) {
